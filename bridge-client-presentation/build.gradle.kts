25c7fed21d61fddf8781e0f77b7851355c758058
Nathaniel Brown <nathaniel.brown@sagebase.org>
2021-03-04 09:27:04 -0800

    Publish presentation module

--- bridge-client-presentation/build.gradle.kts
+++ bridge-client-presentation/build.gradle.kts
@@ -54,3 +54,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
+
+apply("../config/artifact-deploy.gradle")

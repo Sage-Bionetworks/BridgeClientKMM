dacd2d3b84337ed4cf0f4039c07366f9fd1d8e6b
Shannon Young <shannon.young@sagebase.org>
2021-06-08 18:05:56 -0700

    Add SharedMobileUI to the package

--- Package.swift
+++ Package.swift
@@ -20,14 +20,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SageResearch",
-                 url: "https://github.com/Sage-Bionetworks/SageResearch.git",
-                 from: "4.1.0"),
+        .package(name: "SharedMobileUI",
+                 url: "https://github.com/Sage-Bionetworks/SharedMobileUI-AppleOS.git",
+                 from: "0.1.0"),
         .package(name: "BridgeArchiver",
                  url: "https://github.com/Sage-Bionetworks/BridgeArchiver-Swift.git",
                  from: "0.1.0"),
         // TODO: syoung 04/13/2021 Add AssessmentModel-KotlinNative once that repo is set up
         // to build the xcframework when the version is updated.
+        // TODO: syoung 06/08/2021 Add SageResearch once MobileToolbox is set up to use SwiftPM
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -37,8 +38,7 @@ let package = Package(
         .target(name: "BridgeClientUI",
                 dependencies: [
                     "BridgeClient",
-                    .product(name: "Research", package: "SageResearch"),
-                    .product(name: "ResearchUI", package: "SageResearch", condition: .when(platforms: [.iOS])),
+                    "SharedMobileUI",
                     "BridgeArchiver",
                 ],
                 path: "SwiftPackage/Sources/BridgeClientUI")

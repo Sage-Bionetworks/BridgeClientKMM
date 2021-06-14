131119ba1f1832dcfba6ceefbb580fad63f7b324
Nathaniel Brown <nathaniel.brown@sagebase.org>
2021-06-14 15:43:10 -0700

    Fix for running tests on iOS

--- bridge-client/src/iosTest/kotlin/org/sagebionetworks/bridge/kmm/shared/TestUtilsiOS.kt
+++ bridge-client/src/iosTest/kotlin/org/sagebionetworks/bridge/kmm/shared/TestUtilsiOS.kt
@@ -1,9 +1,26 @@
 package org.sagebionetworks.bridge.kmm.shared
 
+import co.touchlab.sqliter.DatabaseConfiguration
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+import com.squareup.sqldelight.drivers.native.wrapConnection
 import org.sagebionetworks.bridge.kmm.shared.cache.BridgeResourceDatabase
+import platform.Foundation.NSUUID
 
-    internal actual fun testDatabaseDriver(): SqlDriver {
-        return NativeSqliteDriver(BridgeResourceDatabase.Schema, "BridgeResourceDatabase")
+internal actual fun testDatabaseDriver(): SqlDriver {
+        val schema = BridgeResourceDatabase.Schema
+        val tableName = NSUUID.UUID().UUIDString //So each test gets own db
+        return NativeSqliteDriver(
+            DatabaseConfiguration(
+                name = tableName,
+                version = schema.version,
+                create = { connection ->
+                    wrapConnection(connection) { schema.create(it) }
+                },
+                upgrade = { connection, oldVersion, newVersion ->
+                    wrapConnection(connection) { schema.migrate(it, oldVersion, newVersion) }
+                },
+                inMemory = true
+            )
+        )
     }

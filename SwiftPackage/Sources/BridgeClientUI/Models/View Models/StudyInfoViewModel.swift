4caf4cba58871dac5c1fcf789d13097c6143968b
Shannon Young <shannon.young@sagebase.org>
2021-08-17 12:18:01 -0700

    Move StudyInfoView from MTB app to shared framework

---
+++ SwiftPackage/Sources/BridgeClientUI/Models/View Models/StudyInfoViewModel.swift
@@ -0,0 +1,225 @@
+//
+//  StudyInfoViewModel.swift
+//
+//  Copyright © 2021 Sage Bionetworks. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2.  Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission. No license is granted to the trademarks of
+// the copyright holders even if such marks are included in this software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import SwiftUI
+import SharedMobileUI
+import BridgeClient
+
+open class StudyInfoViewModel : ObservableObject {
+    
+    @Published open var study: Study! {
+        didSet {
+            updateInfo()
+        }
+    }
+    
+    @Published open var institutionName: String = ""
+    @Published open var logo: Image?
+    @Published open var foregroundColor: Color = .textForeground
+    @Published open var backgroundColor: Color = .accentColor
+    @Published open var title: String = ""
+    @Published open var details: String?
+    @Published open var supportContacts: [StudyContact] = []
+    @Published open var irbContacts: [StudyContact] = []
+    @Published open var studyContacts: [StudyContact] = []
+    
+    @Published open var participantPhone: Phone?
+    @Published open var participantId: String?
+    
+    public init(isPreview: Bool = false) {
+        if isPreview {
+            self.study = previewStudy
+            self.participantId = "123456"
+            updateInfo()
+        }
+    }
+
+    // MARK: Set up
+    
+    open func onAppear(_ bridgeManager: SingleStudyAppManager) {
+        guard let study = bridgeManager.study ?? (bridgeManager.isPreview ? previewStudy : nil)
+        else {
+            return
+        }
+        self.study = study
+        self.participantPhone = bridgeManager.userSessionInfo?.phone
+        self.participantId = bridgeManager.userSessionInfo?.externalId ?? (bridgeManager.isPreview ? "123456" : nil)
+        updateInfo()
+    }
+    
+    open func updateInfo() {
+        self.title = study.name
+        self.details = study.details
+        
+        if let hex = study.colorScheme?.foreground,
+           let color = Color.init(hex: hex) {
+            self.foregroundColor = color
+        }
+        
+        if let hex = study.colorScheme?.background,
+           let color = Color.init(hex: hex) {
+            self.backgroundColor = color
+        }
+           
+        guard let contacts = study.contacts
+        else {
+            return
+        }
+        
+        self.irbContacts = contacts.compactMap { contact in
+            guard contact.role == .irb else { return nil }
+            return StudyContact(contact)
+        }
+        self.supportContacts = contacts.compactMap { contact in
+            guard contact.role == .studySupport else { return nil }
+            return StudyContact(contact)
+        }
+        
+        let studyRoles: [ContactRole] = [.principalInvestigator, .investigator, .sponsor]
+        let studyContacts = contacts
+            .filter { studyRoles.contains($0.role) }
+            .sorted(by: { studyRoles.firstIndex(of: $0.role)! < studyRoles.firstIndex(of: $1.role)! })
+        
+        guard let firstContact = studyContacts.first(where: { $0.affiliation != nil })
+        else {
+            return
+        }
+        
+        self.institutionName = firstContact.affiliation!
+        self.studyContacts = studyContacts.map { StudyContact($0) }
+        self.studyContacts.insert(StudyContact(name: self.institutionName, position: Text("Institution")), at: 1)
+    }
+}
+
+public struct StudyContact : Identifiable {
+    public var id: String { "\(name)|\(position)" }
+    public let name: String
+    public let position: Text
+    public let phone: String?
+    public let email: String?
+    public let isIRB: Bool
+    
+    public init(_ contact: BridgeClient.Contact) {
+        self.name = contact.name
+        self.position = contact.position.map { Text($0) } ?? {
+            switch contact.role {
+            case .investigator:
+                return Text("Investigator")
+            case .irb:
+                return Text("IRB/Ethics Board of Record")
+            case .principalInvestigator:
+                return Text("Principal Investigator")
+            case .sponsor:
+                return Text("Funder")
+            default:
+                return Text("Study Support")
+            }
+        }()
+        self.email = contact.email
+        self.phone = contact.phone.map { $0.nationalFormat ?? $0.number }
+        self.isIRB = (contact.role == .irb)
+    }
+    
+    public init(name: String, position: Text, phone: String? = nil, email: String? = nil, isIRB: Bool = false) {
+        self.name = name
+        self.position = position
+        self.email = email
+        self.phone = phone
+        self.isIRB = isIRB
+    }
+}
+
+let previewStudy = Study(identifier: kPreviewStudyId,
+                         name: "Xcode Preview Study",
+                         phase: StudyPhase.design,
+                         version: 1,
+                         details: "Description about the study. Lorem ipsum about the study written by the research team that they want to share to participants.\n\nLorem ipsum about the study written by the research team that they want to share to participants. Lorem ipsum about the study written by the research team that they want to share to participants.",
+                         clientData: nil,
+                         irbName: "University of San Diego",
+                         irbDecisionOn: nil,
+                         irbExpiresOn: nil,
+                         irbDecisionType: nil,
+                         irbProtocolName: nil,
+                         irbProtocolId: "2039480923",
+                         studyLogoUrl: nil,
+                         colorScheme: nil,
+                         institutionId: nil,
+                         scheduleGuid: nil,
+                         keywords: nil,
+                         diseases: nil,
+                         studyDesignTypes: nil,
+                         signInTypes: nil,
+                         contacts: previewContacts,
+                         deleted: nil,
+                         createdOn: nil,
+                         modifiedOn: nil,
+                         type: nil)
+
+let previewContacts = [
+    Contact(role: ContactRole.studySupport,
+            name: "Christine Larsson",
+            position: "Study Researcher",
+            affiliation: nil,
+            address: nil,
+            email: "CLarsson@institution.edu",
+            phone: Phone(number: "206-555-1212", regionCode: "us", nationalFormat: "(206) 555-1212", type: "Phone"),
+            jurisdiction: nil,
+            type: "Contact"),
+    Contact(role: ContactRole.irb,
+            name: "University of San Diego",
+            position: "IRB/Ethics Board of Record",
+            affiliation: nil,
+            address: nil,
+            email: "institution@institution.edu",
+            phone: Phone(number: "206-555-1212", regionCode: "us", nationalFormat: "(206) 555-1234", type: "Phone"),
+            jurisdiction: nil,
+            type: "Contact"),
+    Contact(role: ContactRole.principalInvestigator,
+            name: "Christine Larsson",
+            position: "Lead Principle Investigator",
+            affiliation: "University of San Diego",
+            address: nil,
+            email: nil,
+            phone: nil,
+            jurisdiction: nil,
+            type: "Contact"),
+    Contact(role: ContactRole.sponsor,
+            name: "National Institute of Aging",
+            position: "Funder",
+            affiliation: nil,
+            address: nil,
+            email: nil,
+            phone: nil,
+            jurisdiction: nil,
+            type: "Contact"),
+]
+
---
+++ SwiftPackage/Sources/BridgeClientUI/Views/StudyInfoView.swift
@@ -0,0 +1,357 @@
+//
+//  StudyInfoView.swift
+//
+//  Copyright © 2021 Sage Bionetworks. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2.  Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission. No license is granted to the trademarks of
+// the copyright holders even if such marks are included in this software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import SwiftUI
+import BridgeClient 
+import SharedMobileUI
+
+public struct StudyInfoView: View {
+    @EnvironmentObject private var bridgeManager: SingleStudyAppManager
+    @StateObject private var viewModel: StudyInfoViewModel = .init()
+    
+    @State private var selectedTab: Tab = .about
+    
+    public init() {
+    }
+    
+    enum Tab : String, CaseIterable, EnumTabItem {
+        case about, contact
+        
+        var bundle: Bundle? { .module }
+
+        func title() -> Text {
+            switch self {
+            case .about:
+                return Text("ABOUT THE STUDY", bundle: .module)
+            case .contact:
+                return Text("CONTACT & SUPPORT", bundle: .module)
+            }
+        }
+    }
+    
+    public var body: some View {
+        CustomTabView(selectedTab: $selectedTab, tabs: Tab.allCases, placement: .top) { tab in
+            ScrollView {
+                switch tab {
+                case .about:
+                    AboutStudyView()
+                case .contact:
+                    ContactAndSupportView()
+                }
+            }
+        }
+        .environmentObject(viewModel)
+        .background(Color.screenBackground.edgesIgnoringSafeArea(.all))
+        .onAppear {
+            viewModel.onAppear(bridgeManager)
+        }
+    }
+}
+
+public struct AboutStudyView: View {
+    @EnvironmentObject private var viewModel: StudyInfoViewModel
+    
+    @State var isPresentingPrivacyNotice: Bool = false
+    @State var privacyNoticeTab: PrivacyNotice.Category = .weWill
+    
+    private let horizontalPadding: CGFloat = 26
+    
+    // MARK: About
+    
+    public var body: some View {
+        VStack(spacing: 0) {
+            mainAboutView()
+            privacyPolicyButton()
+        }
+    }
+    
+    @ViewBuilder
+    private func mainAboutView() -> some View {
+        VStack(alignment: .leading, spacing: 26) {
+            aboutHeader()
+            aboutBody()
+                .foregroundColor(.textForeground)
+            LineView()
+                .padding(.horizontal, horizontalPadding)
+            ForEach(viewModel.studyContacts) { contact in
+                studyInfo(contact)
+                    .padding(.horizontal, horizontalPadding)
+            }
+            Spacer()
+        }
+        .background(Color.sageWhite)
+        .padding(.all, 16)
+    }
+    
+    @ViewBuilder
+    private func aboutHeader() -> some View {
+        if let logo = viewModel.logo {
+            logo
+        }
+        else {
+            Text(viewModel.institutionName.localizedUppercase)
+                .foregroundColor(viewModel.foregroundColor)
+                .padding(.vertical, 28)
+                .padding(.horizontal, horizontalPadding)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(viewModel.backgroundColor)
+        }
+    }
+    
+    @ViewBuilder
+    private func aboutBody() -> some View {
+        Text(viewModel.title)
+            .font(.playfairDisplayFont(18, relativeTo: .title, weight: .regular))
+            .padding(.top, 6)
+            .padding(.horizontal, horizontalPadding)
+        if let details = viewModel.details {
+            Text(details)
+                .font(.latoFont(15, relativeTo: .body, weight: .regular))
+                .fixedSize(horizontal: false, vertical: true)
+                .padding(.horizontal, horizontalPadding)
+        }
+    }
+    
+    @ViewBuilder
+    private func studyInfo(_ contact: StudyContact) -> some View {
+        VStack(alignment: .leading, spacing: 2) {
+            Text(contact.name)
+                .font(.latoFont(14, relativeTo: .title2, weight: .bold))
+            contact.position
+                .font(.latoFont(12, relativeTo: .title3, weight: .regular))
+        }
+        .foregroundColor(.textForeground)
+    }
+    
+    @ViewBuilder
+    private func privacyPolicyButton() -> some View {
+        Button(action: showPrivacyNotice) {
+            Label(
+                title: { Text("Review Privacy Notice", bundle: .module) },
+                icon: { Image("privacy.notice.icon", bundle: .module) }
+            )
+        }
+        .buttonStyle(RoundedButtonStyle(horizontalPadding: 16))
+        .padding(.top, 4)
+        .padding(.horizontal, horizontalPadding)
+        .padding(.bottom, 32)
+        .fullScreenCover(isPresented: $isPresentingPrivacyNotice) {
+            VStack(alignment: .leading, spacing: 0) {
+                Button(action: hidePrivacyNotice) {
+                    Label(
+                        title: { Text("Back", bundle: .module) },
+                        icon: { Image(systemName: "arrow.left") }
+                    )
+                    .font(.latoFont(14, relativeTo: .title2, weight: .regular))
+                    .foregroundColor(.textForeground)
+                    .padding(.horizontal, 16)
+                }
+                PrivacyNoticeView(selectedTab: $privacyNoticeTab)
+            }
+            .background(Color.screenBackground.edgesIgnoringSafeArea(.top))
+        }
+    }
+    
+    private func showPrivacyNotice() {
+        isPresentingPrivacyNotice = true
+    }
+    
+    private func hidePrivacyNotice() {
+        isPresentingPrivacyNotice = false
+    }
+}
+
+public struct ContactAndSupportView: View {
+    @EnvironmentObject private var bridgeManager: SingleStudyAppManager
+    @EnvironmentObject private var viewModel: StudyInfoViewModel
+        
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 25) {
+            Spacer()
+            section(title: Text("General Support", bundle: .module),
+                    body: Text("For general questions about the study or to withdraw from the study, please contact:", bundle: .module),
+                    contacts: viewModel.supportContacts)
+            withdrawalInfoView()
+            LineView()
+            section(title: Text("Your Participant Rights", bundle: .module),
+                    body: Text("For questions about your rights as a research participant, please contact:", bundle: .module),
+                    contacts: viewModel.irbContacts)
+            Spacer()
+        }
+        .padding(.horizontal, 26)
+        .foregroundColor(.textForeground)
+        .background(Color.sageWhite)
+        .padding(.all, 16)
+    }
+    
+    // MARK: Section Header
+    
+    @ViewBuilder
+    private func section(title: Text, body: Text, contacts: [StudyContact]) -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            title
+                .font(.poppinsFont(18, relativeTo: .title2, weight: .regular))
+                .padding(.bottom, 8)
+            body
+                .font(.latoFont(12, relativeTo: .footnote, weight: .regular))
+                .fixedSize(horizontal: false, vertical: true)
+        }
+        .frame(maxWidth: .infinity)
+        
+        ForEach(contacts) {
+            contactView($0)
+                .padding(.horizontal, 16)
+        }
+    }
+    
+    // MARK: Protocol ID
+    
+    private func protocolIdLabel() -> Text {
+        Text("IRB Protocol ID: ", bundle: .module) + Text(viewModel.study?.irbProtocolId ?? "")
+    }
+    
+    // MARK: Contact Info
+    
+    @ViewBuilder
+    private func contactView(_ contact: StudyContact) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 2) {
+                Text(contact.name)
+                    .font(.poppinsFont(14, relativeTo: .title2, weight: .bold))
+                contact.position
+                    .font(.latoFont(12, relativeTo: .title3, weight: .regular))
+            }
+            .padding(.leading, 26)
+            if let phone = contact.phone {
+                contactValue(phone, contactType: .phone)
+            }
+            if let email = contact.email {
+                contactValue(email, contactType: .email)
+            }
+            if contact.isIRB {
+                protocolIdLabel()
+                    .font(.latoFont(12, relativeTo: .title3, weight: .regular))
+                    .padding(.leading, 26)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func contactValue(_ value: String, contactType: ContactType) -> some View {
+        
+        let imageName = contactType.rawValue
+        let label = label(for: contactType)
+        let url = url(value, contactType: contactType)
+        
+        Link(destination: url) {
+            Label(title: {
+                Text(value)
+                    .font(.latoFont(12, relativeTo: .title3, weight: .regular))
+            },
+            icon: {
+                Image("\(imageName).foreground", bundle: .module, label: label)
+                    .background(Image(decorative: "\(imageName).background", bundle: .module)
+                                    .foregroundColor(.accentColor))
+            })
+        }
+    }
+    
+    private func label(for contactType: ContactType) -> Text {
+        switch contactType {
+        case .phone:
+            return Text("Phone", bundle: .module)
+        case .email:
+            return Text("Email", bundle: .module)
+        }
+    }
+    
+    private func url(_ value: String, contactType: ContactType) -> URL {
+        switch contactType {
+        case .phone:
+            return URL(string: "tel:\(value.components(separatedBy: .phoneDigits.inverted).joined())")!
+        case .email:
+            return URL(string: "mailto:\(value)")!
+        }
+    }
+    
+    private enum ContactType : String, CaseIterable {
+        case phone, email
+    }
+    
+    // MARK: Withdrawal Info
+    
+    @ViewBuilder
+    private func withdrawalInfoView() -> some View {
+        VStack(alignment: .leading, spacing: 16) {
+            if let phone = viewModel.participantPhone {
+                withdrawalHeader(Text("To withdraw from this study, you’ll need the Study ID and the phone number you registered with:", bundle: .module))
+                withdrawalRow(Text("Study ID: ", bundle: .module), bridgeManager.studyId!)
+                withdrawalRow(Text("Registration Phone Number: ", bundle: .module), phone.nationalFormat ?? phone.number)
+            }
+            else {
+                withdrawalHeader(Text("To withdraw from this study, you’ll need the following info:", bundle: .module))
+                withdrawalRow(Text("Study ID: ", bundle: .module), bridgeManager.studyId!)
+                withdrawalRow(Text("Participant ID: ", bundle: .module), viewModel.participantId ?? "")
+            }
+        }
+        .font(.latoFont(12, relativeTo: .body, weight: .regular))
+        .padding(.top, 19)
+        .padding(.horizontal, 32)
+        .padding(.bottom, 26)
+        .frame(maxWidth: .infinity)
+        .background(Color.accentColor)
+        .cornerRadius(10)
+    }
+    
+    @ViewBuilder
+    private func withdrawalHeader(_ label: Text) -> some View {
+        label
+            .fixedSize(horizontal: false, vertical: true)
+    }
+    
+    private func withdrawalRow(_ label: Text, _ value: String) -> Text {
+        label.bold() + Text(value)
+    }
+}
+
+extension CharacterSet {
+    static let phoneDigits: CharacterSet = .init(charactersIn: "0123456789")
+}
+
+
+// syoung 08/16/2021 SwiftUI fails to build complex UI in a shared framework. Therefore, the preview
+// for this element is in iosApp.
+struct StudyInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("Hello, World")
+    }
+}

6290fd319d329435e516aa07917ed206a09804f7
Nathaniel Brown <nathaniel.brown@sagebase.org>
2022-04-25 12:27:36 -0700

    Gradle and library updates to work with Android Mobile Toolbox App

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
-        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("com.squareup.sqldelight:gradle-plugin:${Versions.sqlDelight}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("dev.icerock.moko:network-generator:0.16.0")

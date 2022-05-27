b76c224d685511342f8ab0fde988f4a83231c311
Shannon Young <shannon.young@sagebase.org>
2022-05-27 11:01:06 -0700

    Update version to 0.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ tasks.dokkaHtmlMultiModule {
 allprojects {
     group = "org.sagebionetworks.bridge.kmm"
     extra["sdkVersionCode"] = 1
-    version = "0.4.2"
+    version = "0.5.0"
     extra["versionName"] = "android-sdk v${version}"
 
     repositories {

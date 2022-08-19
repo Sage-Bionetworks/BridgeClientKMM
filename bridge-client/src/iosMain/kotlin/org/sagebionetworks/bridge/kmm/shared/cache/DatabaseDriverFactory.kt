baf637e49cd33464a8b6715e4636370280feb06e
Shannon Young <shannon.young@sagebase.org>
2022-08-19 12:41:58 -0700

    Add comment about the database driver

--- bridge-client/src/iosMain/kotlin/org/sagebionetworks/bridge/kmm/shared/cache/DatabaseDriverFactory.kt
+++ bridge-client/src/iosMain/kotlin/org/sagebionetworks/bridge/kmm/shared/cache/DatabaseDriverFactory.kt
@@ -20,6 +20,9 @@ actual class DatabaseDriverFactory: DbDriverFactory {
             return NativeSqliteDriver(schema, name)
         }
         else {
+            // syoung 08/19/2022 This is a copy/paste of the configuration that the above code
+            // uses to create the SQL driver. When updating versions of the SQLite code, this
+            // constructor should be revisited.
             val configuration = DatabaseConfiguration(
                 name = name,
                 version = schema.version,

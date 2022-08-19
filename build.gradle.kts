6d070290c4a2bed93d7ed113be9c8956a6706ed6
Shannon Young <shannon.young@sagebase.org>
2022-08-19 12:45:32 -0700

    Version 0.6.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ tasks.dokkaHtmlMultiModule {
 allprojects {
     group = "org.sagebionetworks.bridge.kmm"
     extra["sdkVersionCode"] = 1
-    version = "0.6.5"
+    version = "0.6.6"
     extra["versionName"] = "android-sdk v${version}"
 
     repositories {

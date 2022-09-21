271fe588cad8d32dd86ddecd98dad99a1fec08c3
Nathaniel Brown <nathaniel.brown@sagebase.org>
2022-09-21 10:29:12 -0700

    Fix for Bridge-3335: don't send duplicate User-Agent strings.

--- bridge-client/src/commonTest/kotlin/org/sagebionetworks/bridge/kmm/shared/TestUtils.kt
+++ bridge-client/src/commonTest/kotlin/org/sagebionetworks/bridge/kmm/shared/TestUtils.kt
@@ -4,10 +4,14 @@ import com.squareup.sqldelight.db.SqlDriver
 import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.engine.mock.*
+import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
 import io.ktor.http.*
+import org.sagebionetworks.bridge.kmm.shared.apis.RefreshTokenFeature
+import org.sagebionetworks.bridge.kmm.shared.apis.SessionTokenFeature
 
 internal expect fun testDatabaseDriver() : SqlDriver
 
@@ -34,6 +38,29 @@ fun getTestClient(mockEngine: HttpClientEngineFactory<MockEngineConfig>) : HttpC
                     println(message)
                 }
             }
+        }
+        install(UserAgent) {
+            agent = "Unit Test agent"
+        }
+        install(SessionTokenFeature) {
+            sessionTokenHeaderName = "Bridge-Session"
+            sessionTokenProvider = object : SessionTokenFeature.SessionTokenProvider {
+
+                override fun getSessionToken(): String? {
+                    return "TestToken"
+                }
+            }
+        }
+        install(RefreshTokenFeature) {
+            updateTokenHandler = suspend {
+                true
+            }
+            isCredentialsActual = fun(request: HttpRequest): Boolean {
+                // By always returning false, test can simulate an expired token by mocking a 401 response.
+                // Other tests will not call this code.
+                return false
+            }
+
         }
     }
 }

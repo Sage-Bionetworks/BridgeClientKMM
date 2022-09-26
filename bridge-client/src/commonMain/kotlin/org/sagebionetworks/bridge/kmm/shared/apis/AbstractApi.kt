0c5d70ba155a1904c42860cf8175587747107365
Nathaniel Brown <nathaniel.brown@sagebase.org>
2022-09-26 13:58:21 -0700

    Add tests for request URL

--- bridge-client/src/commonMain/kotlin/org/sagebionetworks/bridge/kmm/shared/apis/AbstractApi.kt
+++ bridge-client/src/commonMain/kotlin/org/sagebionetworks/bridge/kmm/shared/apis/AbstractApi.kt
@@ -3,6 +3,7 @@ package org.sagebionetworks.bridge.kmm.shared.apis
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 
 internal abstract class AbstractApi(protected val basePath: String, protected val httpClient: HttpClient) {
@@ -13,6 +14,10 @@ internal abstract class AbstractApi(protected val basePath: String, protected va
     }
 
     protected suspend inline fun <reified T, reified S: Any> postData(model: T, path: String) : S {
+        return postDataResponse(model, path).body()
+    }
+
+    internal suspend inline fun <reified T> postDataResponse(model: T, path: String) : HttpResponse {
         val builder = HttpRequestBuilder()
 
         builder.method = HttpMethod.Post
@@ -43,6 +48,14 @@ internal abstract class AbstractApi(protected val basePath: String, protected va
         modifiedDateTimeString: String? = null,
         queryParams: Map<String, String>? = null,
     ): S {
+        return getDataResponse(path, modifiedDateTimeString, queryParams).body()
+    }
+
+    internal suspend fun getDataResponse(
+        path: String,
+        modifiedDateTimeString: String? = null,
+        queryParams: Map<String, String>? = null,
+    ): HttpResponse {
         val builder = HttpRequestBuilder()
 
         builder.method = HttpMethod.Get
@@ -50,7 +63,7 @@ internal abstract class AbstractApi(protected val basePath: String, protected va
             takeFrom(basePath)
             encodedPath = encodedPath.let { startingPath ->
                 path(path)
-                return@let startingPath + encodedPath.substring(1)
+                return@let startingPath + encodedPath
             }
             with(parameters) {
                 queryParams?.let {
@@ -69,7 +82,7 @@ internal abstract class AbstractApi(protected val basePath: String, protected va
         }
 
         try {
-            return httpClient.get(builder).body()
+            return httpClient.get(builder)
         } catch (pipeline: ReceivePipelineException) {
             throw pipeline.cause
         }

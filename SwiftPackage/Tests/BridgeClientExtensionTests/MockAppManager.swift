8e43c42f1aaedfd1d6c5d8bd7a0b0762ff9dd8f0
Shannon Young <shannon.young@sagebase.org>
2023-02-02 10:25:37 -0800

    Mock reauth

--- SwiftPackage/Tests/BridgeClientExtensionTests/MockAppManager.swift
+++ SwiftPackage/Tests/BridgeClientExtensionTests/MockAppManager.swift
@@ -8,7 +8,7 @@
 
 // MARK: MockAuthManager
 class MockAuthManager : NativeAuthenticationManager {
-    fileprivate var mockUserSessionInfo: UserSessionInfo? = UserSessionInfo(firstName: nil, lastName: nil, externalId: nil, id: "not-a-real-user-id", notifyByEmail: nil, attributes: nil, sharingScope: nil, createdOn: nil, emailVerified: nil, phoneVerified: nil, status: nil, roles: nil, dataGroups: nil, clientData: nil, languages: nil, studyIds: ["not-a-real-study-id"], externalIds: nil, authenticated: true, sessionToken: "not-a-real-session-token", reauthToken: nil, environment: nil, email: nil, phone: nil, dataSharing: nil, signedMostRecentConsent: nil, synapseUserId: nil, consented: true, consentStatuses: nil, enrollments: nil, orgMembership: nil, type: "UserSessionInfo")
+    fileprivate var mockUserSessionInfo: UserSessionInfo? = UserSessionInfo(sessionToken: "not-a-real-session-token", reauthToken: "not-a-real-reauth-token")
     
     override func session() -> UserSessionInfo? {
         return mockUserSessionInfo
@@ -18,9 +18,31 @@ class MockAuthManager : NativeAuthenticationManager {
         print("Test would notify UI of Bridge status code \(statusCode) here")
     }
     
-    var mockError: KotlinError?
+    enum MockReauthError : String, Error {
+        case notConnected, invalidReauthToken
+    }
+    
+    var mockReauthError: MockReauthError?
     override func reauth(completion: @escaping (KotlinError?) -> Void) {
+        let mockError = mockReauthError.map {
+            switch $0 {
+            case .invalidReauthToken:
+                mockUserSessionInfo = .init(sessionToken: "", reauthToken: nil)
+            case .notConnected:
+                break
+            }
+            return KotlinError(message: $0.rawValue)
+        }
+        if mockReauthError == nil {
+            mockUserSessionInfo = .init(sessionToken: "different-not-a-real-session-token", reauthToken: "different-not-a-real-reauth-token")
+        }
         completion(mockError)
+    }
+}
+
+extension UserSessionInfo {
+    public convenience init(sessionToken: String, reauthToken: String?) {
+        self.init(firstName: nil, lastName: nil, externalId: nil, id: "not-a-real-user-id", notifyByEmail: nil, attributes: nil, sharingScope: nil, createdOn: nil, emailVerified: nil, phoneVerified: nil, status: nil, roles: nil, dataGroups: nil, clientData: nil, languages: nil, studyIds: ["not-a-real-study-id"], externalIds: nil, authenticated: reauthToken != nil, sessionToken: sessionToken, reauthToken: reauthToken, environment: nil, email: nil, phone: nil, dataSharing: nil, signedMostRecentConsent: nil, synapseUserId: nil, consented: true, consentStatuses: nil, enrollments: nil, orgMembership: nil, type: "UserSessionInfo")
     }
 }
 

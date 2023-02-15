2b6d5ce2a56594d933f7dda2ce468784c1cdf7cc
Shannon Young <shannon.young@sagebase.org>
2023-02-15 13:02:09 -0800

    Version 0.14.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ tasks.dokkaHtmlMultiModule {
 allprojects {
     group = "org.sagebionetworks.bridge.kmm"
     extra["sdkVersionCode"] = 1
-    version = "0.14.0"
+    version = "0.14.1"
     extra["versionName"] = "android-sdk v${version}"
 
     repositories {

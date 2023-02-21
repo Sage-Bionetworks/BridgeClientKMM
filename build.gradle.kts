e575f4a60c9f47ba19ce1920c70a2860097637a9
Nathaniel Brown <nathaniel.brown@sagebase.org>
2023-02-21 10:29:22 -0800

    Version 0.14.2 for Android

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ tasks.dokkaHtmlMultiModule {
 allprojects {
     group = "org.sagebionetworks.bridge.kmm"
     extra["sdkVersionCode"] = 1
-    version = "0.14.1"
+    version = "0.14.2"
     extra["versionName"] = "android-sdk v${version}"
 
     repositories {

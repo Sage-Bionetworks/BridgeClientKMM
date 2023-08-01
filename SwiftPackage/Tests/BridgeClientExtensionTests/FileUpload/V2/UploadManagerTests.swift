eabc2f4f10bd37503d39fbffa993ff7d69110ca0
Shannon Young <shannon.young@sagebase.org>
2023-08-01 16:19:13 -0700

    Add unit test file for UploadManager v2

---
+++ SwiftPackage/Tests/BridgeClientExtensionTests/FileUpload/V2/UploadManagerTests.swift
@@ -0,0 +1,23 @@
+// Created 8/1/23
+// swift-tools-version:5.0
+
+import XCTest
+import BridgeClient
+@testable import BridgeClientExtension
+
+final class UploadManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+
+}
+
+class MockSandboxFileManager : SandboxFileManager {
+}
